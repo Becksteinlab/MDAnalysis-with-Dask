15809756e3e1ca0ca144ee13f4f843b08da2f88e
Mahzad Khoshlessan <mahzad.khoshlessan@asu.edu>
2017-08-31 18:07:31 -0700

    Removing the added submit scripts used for Method1 & Method2- Mistakes in naming

--- Leaflet-finder/submit-scripts/workers.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source /home/mkhoshle/miniconda2/envs/py35/bin/activate py35
-
-echo "CHECKING dask-worker: $(which dask-worker)"
-echo "CHECKING python: $(python --version 2>&1)"
-
-echo "SCHEDULER: $1"
-dask-worker --memory-limit=auto --nprocs 16 --nthreads 1 ${1}:8786
